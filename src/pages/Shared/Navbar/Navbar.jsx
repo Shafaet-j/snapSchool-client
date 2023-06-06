8877cea3a54a5c7f166815c95b816c261e7a6e69
shafaet <lbshaft01@gmail.com>
2023-06-06 22:38:49 +0600

    firebase env setup

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
             <ul className="menu menu-horizontal px-1">{navOptions}</ul>
           </div>
           <div className="navbar-end">
-            <a className="btn">Get started</a>
+            <a className="btn">Profile</a>
           </div>
         </div>
       </>
